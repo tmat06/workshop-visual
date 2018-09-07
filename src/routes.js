acd07573fd6cc07f6608c07e31247466fd7eaf92
tmat06 <timmy.a.matthews@gmail.com>
2018-09-07 17:44:59 -0600

    slide out feature?

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import StyledButtons from "./components/StyledButtons";
+// import StyledButtons from "./components/StyledButtons";
+import SlickExample from "./components/SlickExample";
 import ParallaxExample from "./components/Parallax";
 import ChartJS from "./components/Chartjs";
 import ParticlesJS from "./components/Particles";
@@ -8,7 +9,7 @@ import TestRun from "./components/TestRun";
 
 export default (
   <Switch>
-    <Route exact path="/" component={StyledButtons} />
+    <Route exact path="/" component={SlickExample} />
     <Route path="/parallax" component={ParallaxExample} />
     <Route path="/chart" component={ChartJS} />
     <Route path="/particles" component={ParticlesJS} />
